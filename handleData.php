d9d7e2c5fae016e7ba7b8e25c5b75a163ac2466d
godcheese <godcheese@outlook.com>
2016-01-06 16:42:33 +0800

    Create handleData.php

---
+++ handleData.php
@@ -0,0 +1,17 @@
+<?php
+header('Content-Type:text/html;charset=utf-8');
+$p_username=isset($_POST['p_username'])?$_POST['p_username']:null;
+$p_password=isset($_POST['p_password'])?$_POST['p_password']:null;
+$p_rememberMe=isset($_POST['p_rememberMe'])?$_POST['p_rememberMe']:null;
+
+if(!empty($p_username) && !empty($p_password) && !empty($p_rememberMe)){
+    $p_rememberMe=$p_rememberMe=='true'?$p_rememberMe='记住我已勾选！':$p_rememberMe='记住我未勾选！';
+    echo '{
+"success":true,
+"msg":"'.'登录成功！<br/>用户名：'.$p_username.''.'&nbsp;&nbsp;密码：'.$p_password.'&nbsp;&nbsp;'.$p_rememberMe.'"}';
+
+}else {
+    echo '{"success":false,"msg":"数据提交错误，登录失败！"}';
+}
+
+?>
